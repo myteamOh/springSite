6f32f950b353c2726e7fea1ae242b50ced9eb0ce
GeeHun Oh <ohgeehun90@naver.com>
2018-01-24 13:54:25 +0900

    파일 업로드 완료

--- springSite/src/main/webapp/resources/include/js/common.js
+++ springSite/src/main/webapp/resources/include/js/common.js
@@ -65,7 +65,7 @@ function chkFile(item) {
 	/*
 	 * 배열내의 값을 찾아서 인덱스를 반환(요소가 없을 경우 -1 반환) jQuery.inArray(찾을 값, 검색 대상의 배열)
 	 */
-	var ext = item.val.split('.').pop().toLowerCase();
+	var ext = item.val().split('.').pop().toLowerCase();
 	if (jQuery.inArray(ext, [ 'gif', 'png', 'jpg', 'jpeg' ]) == -1) {
 		alert('gif, png, jpg, jpeg 파일만 업로드 할 수 있습니다.');
 		return false;
