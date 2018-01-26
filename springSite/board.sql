809fb2ff8c36f8d0cbff3c448b552e194236f859
GeeHun Oh <ohgeehun90@naver.com>
2018-01-26 11:43:22 +0900

    board.sql 회원테이블 생성

--- springSite/board.sql
+++ springSite/board.sql
@@ -82,3 +82,57 @@ COMMENT ON COLUMN spring_gallery.g_date is '갤러리게시판 등록일';
 -- 갤러리 시퀀스 생성 by.moon
 CREATE SEQUENCE spring_gallery_seq;
 
+-- 회원가입 테이블
+CREATE TABLE SPRING_MEMBER(
+	IDX NUMBER,
+	USERID VARCHAR2(70) NOT NULL,
+	USERPW VARCHAR2(100) NOT NULL,
+	USERNAME VARCHAR2(50) NOT NULL,
+	PINNO VARCHAR2(15) NOT NULL,
+	EMAIL VARCHAR2(70) NOT NULL,
+	PHONE VARCHAR2(13) NOT NULL,
+	JOINDATE DATE DEFAULT SYSDATE,
+	CONSTRAINT SPRING_MEMBER_PK PRIMARY KEY(IDX),
+	CONSTRAINT SPRING_MEMBER_UK UNIQUE(USERID)
+);
+
+comment on table    spring_member is '회원 테이블 정보'; 
+comment on column spring_member.idx is '회원테이블 순번';
+comment on column spring_member.userid is '회원테이블 아이디';
+comment on column spring_member.userpw is '회원테이블 비밀번호';
+comment on column spring_member.username is '회원테이블 이름';
+comment on column spring_member.pinno is '회원테이블 생년월일';
+comment on column spring_member.email is '회원테이블 이메일'; 
+comment on column spring_member.phone is '회원테이블 핸드폰번호'; 
+comment on column spring_member.joindate is '회원테이블 등록일';  
+
+-- 회원 가입시 사용할 회원번호(시퀀스)
+CREATE SEQUENCE SPRING_MEMBER_SEQ;
+
+-- 해시함수 솔트값을 저장하기 위한 테이블(비밀번호 암호화)
+CREATE TABLE SECURITY(
+	USERID VARCHAR2(70),
+	SALT VARCHAR2(70),
+	CONSTRAINT SECURITY_PK PRIMARY KEY(USERID)
+);
+
+-- 로그인 정보 저장 테이블
+CREATE TABLE LOGIN_HISTORY(
+	idx NUMBER, userid VARCHAR2(70),
+	retry NUMBER DEFAULT 0,
+	lastFailedLogin NUMBER DEFAULT 0,
+	lastSuccessedLogin NUMBER DEFAULT 0,
+	clientIp VARCHAR2(15),
+	CONSTRAINT login_history_pk PRIMARY KEY(idx) 
+);
+
+COMMENT ON TABLE login_history is '로그인 정보 저장 테이블'; 
+COMMENT ON COLUMN login_history.idx is '순번';
+COMMENT ON COLUMN login_history.userid is '로그인 아이디';
+COMMENT ON COLUMN login_history.retry is '로그인 시도 횟수'; 
+COMMENT ON COLUMN login_history.lastfailedlogin is '마지막으로 실패한 로그인 시간'; 
+COMMENT ON COLUMN login_history.lastsuccessedlogin is '마지막으로 성공한 로그인 시간'; 
+COMMENT ON COLUMN login_history.clientip is '로그인한 사용자의 ip 주소'; 
+
+-- 로그인 정보 저장시 사용할 순번(시퀀스)
+CREATE SEQUENCE LOGIN_HISTORY_SEQ;
